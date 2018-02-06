f3c6fb0e8df973e669fe8c982f4228e48195d2b8
Ruslan Chepizhko <chepizhko.ruslan@gmail.com>
2018-02-06 18:51:12 +0200

    add Box

--- app/src/main/java/com/chepizhko/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/chepizhko/draganddraw/BoxDrawingView.java
@@ -8,8 +8,14 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+
     // Используется при создании представления в коде
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -27,15 +33,31 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                // Сброс текущего состояния
+                // При каждом получении события ACTION_DOWN в поле mCurrentBox сохраняется новый объект Box с базовой точкой,
+                // соответствующей позиции события
+                mCurrentBox = new Box(current);
+                // объект Box добавляется в массив прямоугольников
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    // В процессе перемещения пальца по экрану приложение обновляет mCurrentBox.mCurrent
+                    mCurrentBox.setCurrent(current);
+                    // invalidate() заставляет BoxDrawingView перерисовать себя, чтобы пользователь видел прямоугольник в процессе перетаскивания
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                // поле mCurrentBox обнуляется для завершения операции
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                // поле mCurrentBox обнуляется для завершения операции
+                mCurrentBox = null;
                 break;
         }
         Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
