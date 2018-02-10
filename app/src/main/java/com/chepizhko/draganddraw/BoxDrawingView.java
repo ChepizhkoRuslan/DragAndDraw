33e885bb592856ce58401fcfafd9deaa3525691a
Ruslan Chepizhko <chepizhko.ruslan@gmail.com>
2018-02-10 10:24:15 +0200

    add method onDraw

--- app/src/main/java/com/chepizhko/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/chepizhko/draganddraw/BoxDrawingView.java
@@ -2,6 +2,8 @@ package com.chepizhko.draganddraw;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -15,6 +17,10 @@ public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
+    // Методы, вызываемые для объекта Paint, определяют характеристики вывода: должны ли фигуры заполняться,
+    // каким шрифтом должен выводиться текст, каким цветом должны выводиться линии и т. д.
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     // Используется при создании представления в коде
     public BoxDrawingView(Context context) {
@@ -23,7 +29,30 @@ public class BoxDrawingView extends View {
     // Используется при заполнении представления по разметке XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        // Прямоугольники рисуются полупрозрачным красным цветом (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+        // Фон закрашивается серовато-белым цветом
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Заполнение фона
+        canvas.drawPaint(mBackgroundPaint);
+        for (Box box : mBoxen) {
+            // для каждого прямоугольника в списке мы определяем значения left, right, top и bottom по двум точкам.
+            // Значения left и top будут минимальными, а bottom и right — максимальными
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            // После вычисления параметров вызов метода Canvas.drawRect(…) рисует красный прямоугольник на экране
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
